613b32bad076b7f94dc9f8749f1474c1add56109
AntonioCassian <antoniomart849@gmail.com>
2024-01-29 22:08:55 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "../../assets/Logo";
+import { FaUser } from 'react-icons/fa'
 import './style.scss'
 
 export const Header = () => {
@@ -8,11 +9,15 @@ export const Header = () => {
                 <div className="container-main">
                     <nav className="nav">
                         <div className="nav-left">
-                            <div className="logo">
-                                <Logo />
-                            </div>
+                            <a href="#" className="link"><Logo /></a>
+                            <a href="#" className="link">Home</a>
+                            <a href="#" className="link">Toyota</a>
+                            <a href="#" className="link">Volkswage</a>
+                            <a href="#" className="link">Fiat</a>
+                            <a href="#" className="link">Chevrolet</a>
+                            <a href="#" className="link">Hiunday</a>
                         </div>
-                        <div className="nav-rigt"></div>
+                        <div className="nav-rigt"><FaUser /></div>
                     </nav>
                 </div>
             </header>
