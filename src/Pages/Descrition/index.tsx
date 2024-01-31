5774f536b40db37103587a433291432b3896b425
AntonioCassian <antoniomart849@gmail.com>
2024-01-31 11:46:26 -0300

    feat: create page desc

--- src/Pages/Descrition/index.tsx
+++ src/Pages/Descrition/index.tsx
@@ -11,7 +11,15 @@ export const Descriton = () => {
                 <section className="sec-desc">
                 <Desc />
                 <form action="">
-                    
+                    <label htmlFor="retirada">Data de retirada</label>
+                    <input type="date" name="" id="retirada" />
+
+                    <label htmlFor="volta">Data de volta</label>
+                    <input type="date" name="" id="volta" />
+
+                    <button className="btn">
+                        Alugar
+                    </button>
                 </form>
                 </section>
             </div>
