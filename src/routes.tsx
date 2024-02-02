b7695a6679afb5aec4d002a6996c10579ee3260f
AntonioCassian <antoniomart849@gmail.com>
2024-02-01 22:41:45 -0300

    feat: page cars

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Descriton } from "./Pages/Descrition";
+import { Cars } from "./Pages/Cars";
 
 export const Routs = () => {
     return (
@@ -8,6 +9,7 @@ export const Routs = () => {
             <Routes>
                 <Route path="/"  element={<Home/>} />
                 <Route path="/des"  element={<Descriton />} />
+                <Route path="/cars"  element={<Cars />} />
             </Routes>
         </Router>
     )
