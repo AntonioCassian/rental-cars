c3a7fc2ca3446c711435e89042559df6caa364a8
AntonioCassian <antoniomart849@gmail.com>
2024-02-06 21:34:24 -0300

    feat: page desc

--- src/Pages/Descrition/index.tsx
+++ src/Pages/Descrition/index.tsx
@@ -1,34 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Desc } from "../../components/Desc";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import './style.scss'
+import { api } from "../../Provider/api";
 
 export const Descriton = () => {
+    const { id } = useParams();
+    const [car, setCar] = useState([]);
+
+     useEffect(() => {
+        api.get(`/cars/${id}`)
+        .then((response) => {
+            setCar( response.data)
+        })
+        .catch(()=> {
+            alert('Erro ao consultar ao BD');
+        })
+    }, [id]);
+    console.log(id)
+     
     return (
         <>
             <Header />
             <div className="container">
                 <section className="sec-desc">
-                <Desc />
-                <form action="">
-                    <label htmlFor="retirada">Data de retirada</label>
-                    <input type="date" name="" id="retirada" />
 
-                    <label htmlFor="volta">Data de volta</label>
-                    <input type="date" name="" id="volta" />
+                    <Desc image={undefined} modelo={undefined} nome={undefined} description={undefined} {...car} />
+                    <form action="">
+                        <label htmlFor="retirada">Data de retirada</label>
+                        <input className="inp-dat" type="date" name="" id="retirada" />
 
-                    <div className="result">
-                        <div className="res">
-                            50,00
+                        <label htmlFor="volta">Data de volta</label>
+                        <input className="inp-dat" type="date" name="" id="volta" />
+
+                        <div className="result">
+                            <input type="number" className="res" placeholder="50,00" />
+                                
+                            <div className="res">
+                                500,00
+                            </div>
                         </div>
-                        <div className="res">
-                            500,00
-                        </div>
-                    </div>
-                    <button className="btn">
-                        Alugar
-                    </button>
-                </form>
+                        <button className="btn">
+                            Alugar
+                        </button>
+                    </form>
                 </section>
             </div>
             <Footer />
--- src/components/Desc/index.tsx
+++ src/components/Desc/index.tsx
@@ -1,17 +1,19 @@
-import Carro from '../../assets/carro_toyota.png'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+//import Carro from '../../assets/carro_toyota.png'
+
 import './style.scss'
 
-export const Desc = () => {
+
+
+export const Desc = (car: { image: string | undefined; modelo: string | undefined; nome: string | undefined, description: string | undefined }) => {
     return (
         <>
             <div className="desc">
                 <div className="image-desc">
-                    <img src={Carro} />
-                    <h4 className='title-desc'>Toyota - Corolla 2021</h4>
+                    <img src={car.image} />
+                    <h4 className='title-desc'>{car.modelo} - {car.nome}</h4>
                     <p>
-                        Com o novo motor híbrido o Toyota Corolla 2021 tem à sua disposição 122 cavalos de potência máxima contra 177 cavalos do motor flex.
-                        Pode parecer uma grande diferença, mas o motor híbrido aciona a parte elétrica em rotações mais baixas fazendo com que o torque entregue em retomadas e acelerações seja muito parecido com o do Corolla Flex.
-                        Se, no desempenho a diferença é pequena, no consumo ela é muito grande e obviamente em favor do Toyota Corolla 2021 híbrido, com o sedan chegando a rodar 14,5 km/litro de gasolina na cidade e 16,3 km/litro na estrada, contra 9,7 e 13,9 km/litro do Corolla Flex.
+                        {car.description}
                     </p>
                 </div>
             </div>
