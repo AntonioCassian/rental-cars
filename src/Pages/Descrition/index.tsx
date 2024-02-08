242da6703c27f1727115fd6f838b0a6d9aa1a842
AntonioCassian <antoniomart849@gmail.com>
2024-02-08 10:02:47 -0300

    feat: create form

--- src/Pages/Descrition/index.tsx
+++ src/Pages/Descrition/index.tsx
@@ -10,17 +10,17 @@ export const Descriton = () => {
     const { id } = useParams();
     const [car, setCar] = useState([]);
 
-     useEffect(() => {
+    useEffect(() => {
         api.get(`/cars/${id}`)
-        .then((response) => {
-            setCar( response.data)
-        })
-        .catch(()=> {
-            alert('Erro ao consultar ao BD');
-        })
+            .then((response) => {
+                setCar(response.data)
+            })
+            .catch(() => {
+                alert('Erro ao consultar ao BD');
+            })
     }, [id]);
     console.log(id)
-     
+
     return (
         <>
             <Header />
@@ -28,24 +28,54 @@ export const Descriton = () => {
                 <section className="sec-desc">
 
                     <Desc image={undefined} modelo={undefined} nome={undefined} description={undefined} {...car} />
-                    <form action="">
-                        <label htmlFor="retirada">Data de retirada</label>
-                        <input className="inp-dat" type="date" name="" id="retirada" />
+                    <div className="d-flex-col">
+                        <form action="">
+                            <label htmlFor="retirada">Data de retirada</label>
+                            <input className="inp-dat" type="date" name="" id="retirada" />
 
-                        <label htmlFor="volta">Data de volta</label>
-                        <input className="inp-dat" type="date" name="" id="volta" />
+                            <label htmlFor="volta">Data de volta</label>
+                            <input className="inp-dat" type="date" name="" id="volta" />
 
-                        <div className="result">
-                            <input type="number" className="res" placeholder="50,00" />
-                                
-                            <div className="res">
-                                500,00
+                            <div className="result">
+                                <input type="number" className="res iptn" placeholder="50,00" />
+
+                                <div className="res">
+                                    500,00
+                                </div>
+                            </div>
+                            <button className="btn">
+                                Alugar
+                            </button>
+                        </form>
+
+                        <div className="pagamento">
+                            <div className="credito">
+                                <form className="form-cartao">
+                                    <label htmlFor="numero" className="label-cartao">N° do Cartão</label>
+                                    <input type="number" id="numero" className="iptn inpt-cartao" />
+
+                                    <label htmlFor="nome">Nome</label>
+                                    <input type="text" id="nome" className="iptn-n" />
+
+                                    <div className="valid-cvv">
+                                        <span>
+                                            <label htmlFor="validad">Validade</label>
+                                            <input type="number" id="validad" className="iptn" />
+                                        </span>
+
+                                        <span>
+                                            <label htmlFor="cvv">CVV</label>
+                                            <input type="number" id="cvv" className="iptn" />
+                                        </span>
+                                    </div>
+
+                                    <button className="btn">
+                                        Salvar
+                                    </button>
+                                </form>
                             </div>
                         </div>
-                        <button className="btn">
-                            Alugar
-                        </button>
-                    </form>
+                    </div>
                 </section>
             </div>
             <Footer />
